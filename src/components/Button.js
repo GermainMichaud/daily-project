b95eb1f7acfca4b9f0a898584b605c635a4fdb3f
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 01:57:26 +0200

    :sparkles: Create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button((props) => ({
+  padding: '.5rem 1rem',
+  color: '#fff',
+  fontSize: '1rem',
+  fontWeight: 600,
+  textTransform: 'uppercase',
+  letterSpacing: '1px',
+  border: 'none',
+  backgroundColor: '#c75e2c',
+}));
+
+export default Button;
