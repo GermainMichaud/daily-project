8772d8d9db17e03adb80200e2d83e12095f89d0f
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 01:58:34 +0200

    :sparkles: Create Content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Content = styled.main((props) => ({
   display: 'flex',
   flexDirection: props.direction || 'column',
+  padding: '2rem 3rem',
 }));
 
 export default Content;
