57f421ba3744195c511ace15a60adde064c17c97
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 02:00:23 +0200

    :sparkles: Create Comments component

---
+++ src/components/Comments.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Comments = styled.section((props) => ({
+  position: 'relative',
+  paddingBottom: props.paddingBottom,
+}));
+
+export default Comments;
