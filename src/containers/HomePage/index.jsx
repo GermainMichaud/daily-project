6b50eabd249c97cb0ffc5763643b45017af25f95
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 02:03:42 +0200

    :sparkles: Create HomePage component

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from '../../components/Button';
+import Form from '../../components/Form';
+import Grid from '../../components/Grid';
+import Input from '../../components/Input';
+import useProjects from '../../hooks/useProjects';
+import Project from './Project';
 
-const index = () => <div />;
+const HomePage = () => {
+  const { storedValue, setValue, remove } = useProjects('projects', []);
+  const [projectName, setProjectName] = useState('');
 
-export default index;
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!projectName.trim().length) return;
+    setValue(projectName);
+    setProjectName('');
+  };
+
+  useEffect(() => {
+    console.log('storedValue changed', storedValue);
+  }, [storedValue]);
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <Input
+          type="text"
+          name="projectName"
+          id="projectName"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+          placeholder="Nouveau projet"
+        />
+        <Button>Enregistrer</Button>
+      </Form>
+      <Grid>
+        {storedValue.length ? (
+          storedValue.map((project) => (
+            <Project key={project.id} project={project} remove={remove} />
+          ))
+        ) : (
+          <p>Aucun projet pour le moment</p>
+        )}
+      </Grid>
+    </>
+  );
+};
+
+export default HomePage;
