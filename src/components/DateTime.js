1156d8613e0e45e7ce8c8efac20f811677c939f6
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 02:00:05 +0200

    :sparkles: Create DateTime component

---
+++ src/components/DateTime.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const DateTime = styled.span({
+  position: 'absolute',
+  bottom: '10px',
+  right: '10px',
+  fontSize: '.6rem',
+  color: 'var(--fontColor)',
+});
+
+export default DateTime;
