69c4dcc9e51fe6b6ef2cc2bf9903d19e6ac99763
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-13 02:05:01 +0200

    :sparkles: Add Routes and update structure

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import AppContainer from '../../components/AppContainer';
 import Header from '../../components/Header';
 import logo from '../../assets/logo.svg';
 import './App.css';
 import Brand from '../../components/Brand';
 import Content from '../../components/Content';
+import HomePage from '../HomePage';
+import ProjectPage from '../ProjectPage';
 
 function App() {
   return (
     <AppContainer>
-      <Header>
-        <Brand>
-          <img src={logo} height="30" alt="Logo" />
-          Daily Project
-        </Brand>
-      </Header>
-      <Content />
+      <Router>
+        <Header>
+          <Brand>
+            <img src={logo} height="30" alt="Logo" />
+            Daily Project
+          </Brand>
+          <Link to="/">Projects</Link>
+        </Header>
+        <Content>
+          <Route
+            render={({ location }) => (
+              <Switch location={location} key={location.pathname}>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/:projectName" component={ProjectPage} />
+              </Switch>
+            )}
+          />
+        </Content>
+      </Router>
     </AppContainer>
   );
 }
